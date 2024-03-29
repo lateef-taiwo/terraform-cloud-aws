3497859478682148e51e27c273242208f5351a68
Lateef Taiwo <taiwolateef55@gmail.com>
2024-03-29 21:08:35 +0100

    modified:   modules/RDS/rds.tf

--- modules/RDS/rds.tf
+++ modules/RDS/rds.tf
@@ -18,14 +18,14 @@ resource "aws_db_instance" "cloudops-rds" {
   storage_type           = "gp2"
   engine                 = "mysql"
   engine_version         = "5.7"
-  instance_class         = "db.t2.micro"
+  instance_class         = "db.t3.micro" # Updated instance class
   db_name                = "savvydb"
-  username               = var.db-username
-  password               = var.db-password
+  username               = var.db-username # Ensure variable names match Terraform syntax
+  password               = var.db-password # Ensure variable names match Terraform syntax
   parameter_group_name   = "default.mysql5.7"
   db_subnet_group_name   = aws_db_subnet_group.cloudops-rds.name
   skip_final_snapshot    = true
-  vpc_security_group_ids = var.db-sg
+  vpc_security_group_ids = var.db-sg # Ensure variable names match Terraform syntax
   multi_az               = "true"
 
 }
