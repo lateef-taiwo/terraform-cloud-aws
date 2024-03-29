15c5106a6b7528dbd3c4ec396641795e6690bc7e
Lateef Taiwo <taiwolateef55@gmail.com>
2024-03-29 01:41:56 +0100

    modified:   modules/ALB/cert.tf

--- modules/ALB/cert.tf
+++ modules/ALB/cert.tf
@@ -1,21 +1,21 @@
 # The entire section create a certiface, public zone, and validate the certificate using DNS method
 
 # Create the certificate using a wildcard for all the domains created in mytoolz
-resource "aws_acm_certificate" "akoji_cert" {
-  domain_name       = "*.akoji.site"
+resource "aws_acm_certificate" "cloudopsdomain_cert" {
+  domain_name       = "*.cloudopsdomain.online"
   validation_method = "DNS"
 }
 
 # calling the hosted zone
-data "aws_route53_zone" "akoji_zone" {
-  name         = "akoji.site"
+data "aws_route53_zone" "cloudopsdomain_zone" {
+  name         = "cloudopsdomain.online"
   private_zone = false
 }
 
 # selecting validation method
-resource "aws_route53_record" "akoji_record" {
+resource "aws_route53_record" "cloudopsdomain_record" {
   for_each = {
-    for dvo in aws_acm_certificate.akoji_cert.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.cloudopsdomain_cert.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
@@ -27,19 +27,19 @@ resource "aws_route53_record" "akoji_record" {
   records         = [each.value.record]
   ttl             = 60
   type            = each.value.type
-  zone_id         = data.aws_route53_zone.akoji_zone.zone_id
+  zone_id         = data.aws_route53_zone.cloudopsdomain_zone.zone_id
 }
 
 # validate the certificate through DNS method
-resource "aws_acm_certificate_validation" "akoji" {
-  certificate_arn         = aws_acm_certificate.akoji_cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.akoji_record : record.fqdn]
+resource "aws_acm_certificate_validation" "cloudopsdomain" {
+  certificate_arn         = aws_acm_certificate.cloudopsdomain_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cloudopsdomain_record : record.fqdn]
 }
 
 # create records for tooling
 resource "aws_route53_record" "tooling" {
-  zone_id = data.aws_route53_zone.akoji_zone.zone_id
-  name    = "tooling.akoji.site"
+  zone_id = data.aws_route53_zone.cloudopsdomain_zone.zone_id
+  name    = "tooling.cloudopsdomain.site"
   type    = "A"
 
   alias {
@@ -51,8 +51,8 @@ resource "aws_route53_record" "tooling" {
 
 # create records for wordpress
 resource "aws_route53_record" "wordpress" {
-  zone_id = data.aws_route53_zone.akoji_zone.zone_id
-  name    = "wordpress.akoji.site"
+  zone_id = data.aws_route53_zone.cloudopsdomain_zone.zone_id
+  name    = "wordpress.cloudopsdomain.site"
   type    = "A"
 
   alias {
