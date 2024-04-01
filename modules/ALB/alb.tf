827eaa6cf50bdc8a5eabbb5c72b891b11798e6f0
Lateef Taiwo <taiwolateef55@gmail.com>
2024-04-01 15:26:02 +0100

    modified:   modules/ALB/alb.tf         modified:   modules/Autoscaling/asg-bastion-nginx.tf         modified:   modules/Autoscaling/asg-wordpress-tooling.tf

--- modules/ALB/alb.tf
+++ modules/ALB/alb.tf
@@ -113,46 +113,46 @@ resource "aws_lb_target_group" "tooling-tgt" {
 
 #create a Listener for the load balancer
 
-resource "aws_lb_listener" "nginx-listener" {
-  load_balancer_arn = aws_lb.ext-alb.arn
-  port              = 443
-  protocol          = "HTTPS"
-  certificate_arn   = aws_acm_certificate_validation.cloudopsdomain.certificate_arn
+# resource "aws_lb_listener" "nginx-listener" {
+#   load_balancer_arn = aws_lb.ext-alb.arn
+#   port              = 443
+#   protocol          = "HTTPS"
+#   certificate_arn   = aws_acm_certificate_validation.cloudopsdomain.certificate_arn
 
-  default_action {
-    type             = "forward" 
-    target_group_arn = aws_lb_target_group.nginx-tgt.arn
-  }
-}
+#   default_action {
+#     type             = "forward" 
+#     target_group_arn = aws_lb_target_group.nginx-tgt.arn
+#   }
+# }
 
 
 #For this aspect a single listener was created for the wordpress which is default,
 #A rule was created to route traffic to tooling when the host header changes
 
-resource "aws_lb_listener" "web-listener" {
-  load_balancer_arn = aws_lb.ialb.arn
-  port              = 443
-  protocol          = "HTTPS"
-  certificate_arn   = aws_acm_certificate_validation.cloudopsdomain.certificate_arn
+# resource "aws_lb_listener" "web-listener" {
+#   load_balancer_arn = aws_lb.ialb.arn
+#   port              = 443
+#   protocol          = "HTTPS"
+#   certificate_arn   = aws_acm_certificate_validation.cloudopsdomain.certificate_arn
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.wordpress-tgt.arn
-  }
-}
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.wordpress-tgt.arn
+#   }
+# }
 
-resource "aws_lb_listener_rule" "tooling-listener" {
-  listener_arn = aws_lb_listener.web-listener.arn
-  priority     = 99
+# resource "aws_lb_listener_rule" "tooling-listener" {
+#   listener_arn = aws_lb_listener.web-listener.arn
+#   priority     = 99
 
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.tooling-tgt.arn
-  }
+#   action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.tooling-tgt.arn
+#   }
 
-  condition {
-    host_header {
-      values = ["tooling.cloudopsdomain.online"]
-    }
-  }
-}
+#   condition {
+#     host_header {
+#       values = ["tooling.cloudopsdomain.online"]
+#     }
+#   }
+# }
